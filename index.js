f1d8db2601477f5f38861efc3fafa395e4bbd187
sreyamanoharan <sreyampk@gmail.com>
2024-08-17 14:26:49 +0530

    profile updatessssssssss

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const dbUrl = 'mongodb+srv://aswinachuz894:Aswinmongodbatles@cluster0.cvp4dws.mongodb.net/edunova';
 
 app.use(cors({
-   origin: ["http://localhost:5173"],
+   origin: ["http://localhost:5173",'https://edunova-puce.vercel.app'],
    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 }));
 app.use(express.json()); 
