8d3dc797dbb3b16237801e2a6432714f887afd09
sreyamanoharan <sreyampk@gmail.com>
2024-08-17 13:52:21 +0530

    profile update

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -36,6 +36,7 @@ export const allMembers=async(req,res)=>{
   try { 
     const Members=await Edunova.find()
     res.status(200).json({Members}) 
+    
   } catch (error) {
     console.log(error);
   }
@@ -44,7 +45,6 @@ export const allMembers=async(req,res)=>{
 export const deleteMember = async (req, res) => {
   try {
     const memberId = req.params.id;
-    console.log(memberId);
     
     await Edunova.findByIdAndDelete(memberId); 
     res.status(200).json({ message: 'Member deleted successfully' });
@@ -53,4 +53,61 @@ export const deleteMember = async (req, res) => {
   }
 };
 
+// export const UpdateMember=async(req,res)=>{
+//   const { id } = req.params;
+//   const { name, email, role, status, teams, profilePicture } = req.body;
+//   try {
+   
+//     console.log(memberId,'uuuuuuuuuuuuuu');
+    
 
+//     const updatedMember = await Edunova.findByIdAndUpdate(memberId, updateData, {
+//       new: true, 
+//       runValidators: true, 
+//     });
+
+//     if (!updatedMember) {
+//       return res.status(404).json({ message: 'Member not found' });
+//     }
+
+//     res.json({ message: 'Member updated successfully', member: updatedMember });
+//   } catch (error) {
+//     console.error('Error updating member:', error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// }
+
+
+export const UpdateMember= async (req, res) => {
+  console.log('hi hereeeee');
+  
+  const { id } = req.params;
+  const { name, email, role, status, teams, profilePicture } = req.body;
+  console.log(req.body,'pppppppppppppppppppppppqqqqqqqqqqqqq');
+  
+
+  try {
+    const updatedUser = await Edunova.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        role,
+        status,
+        teams,
+        profilePicture
+      },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+
+}
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -3,10 +3,13 @@ const userRouter = express.Router();
 import { memberReg } from "../Controllers/Controller.js";
 import { allMembers } from "../Controllers/Controller.js";
 import { deleteMember } from "../Controllers/Controller.js";
+import { UpdateMember } from "../Controllers/Controller.js";
 
 userRouter.post('/register',memberReg)
 userRouter.get('/members',allMembers)
 userRouter.delete('/members/:id',deleteMember );
+userRouter.put('/members/:id',UpdateMember)
+
 
 
 
